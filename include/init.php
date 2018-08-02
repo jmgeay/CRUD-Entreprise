5c39c777295e3bf7cbbbdac60ba7b00a3b255d9a
Jean-Marc Geay <redjanus@gmail.com>
2018-08-02 16:22:54 +0200

    travail_jm

---
+++ include/init.php
@@ -0,0 +1,12 @@
+<?php
+  //connexion bdd
+  $pdo = new PDO('mysql:host=localhost;dbname=entreprise', 'root', '', array
+  (PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+  //session
+  session_start();
+
+  //variables
+  $content = '';
+  $error = '';
+?>
